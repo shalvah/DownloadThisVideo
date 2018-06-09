2723904d34fc87d128e09e398cc9226b89a43f7b
shalvah <diakon.ng@gmail.com>
2018-06-09 03:35:39 +0100

    Refactor tweet ops

---
+++ src/services/factory.twitter.js
@@ -0,0 +1,121 @@
+"use strict";
+
+const {
+    not ,
+    and,
+    pluck,
+    randomSuccessResponse,
+} = require('../utils');
+const { TwitterErrorResponse } = require('../errors');
+const {
+    haveIRepliedToTweetAlready,
+    isTweetAReplyToMe,
+    isTweetAReply
+} = require('./tweet_operations');
+const Twit = require('twit');
+
+const t = new Twit({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+module.exports = (cache) => {
+
+    const getMentions = async () => {
+        let lastTweetId = await cache.getAsync('lastTweetRetrieved');
+        let options = {count: 100};
+        if (lastTweetId) {
+            options.since_id = lastTweetId;
+        }
+        return t.get('statuses/mentions_timeline', options)
+            .then(r => {
+                if (r.data.errors) {
+                    throw `Error in statuses/mentions_timeline response: ${JSON.stringify(r.data.errors)}`;
+                }
+                return r.data;
+            })
+            .then(tweets => tweets.filter(and(isTweetAReply, not(isTweetAReplyToMe))))
+            .then(tweets => tweets.map(tweetObject => {
+                return {
+                    id: tweetObject.id_str,
+                    referencing_tweet: tweetObject.in_reply_to_status_id_str,
+                    author: tweetObject.user.screen_name
+                }
+            }));
+    };
+
+    const getTweets = (tweets) => {
+        return t.post(`statuses/lookup`, {
+            id: pluck(tweets, 'referencing_tweet'),
+            tweet_mode: 'extended',
+        }).then(r => {
+            if (r.data.errors) {
+                throw new TwitterErrorResponse('statuses/lookup', r.data.errors);
+            }
+            return r.data;
+        });
+    };
+
+    const reply = (tweet, content) => {
+        let options = {
+            in_reply_to_status_id: tweet.id,
+            status: `@${tweet.author} ${content}`
+        };
+        return t.post('statuses/update', options)
+            .then(r => {
+                if (r.data.errors) {
+                    throw new TwitterErrorResponse('statuses/update', r.data.errors);
+                }
+                return r;
+            });
+    };
+
+    const replyWithLink = (tweet, link) => {
+        let content = `${randomSuccessResponse()} ${link}`;
+        return reply(tweet, content);
+    };
+
+    const getMyTweets = async () => {
+        let myTweets = await cache.lrangeAsync('myTweets', 0, -1);
+        if (myTweets.length) {
+            return myTweets;
+        }
+
+        return t.get(`statuses/user_timeline`, {
+            screen_name: process.env.TWITTER_SCREEN_NAME,
+            include_rts: false,
+            count: 200,
+        }).then(r => {
+            myTweets = r.data.map(t => t.in_reply_to_status_id_str);
+            return cache.lpushAsync('myTweets', myTweets)
+        }).then(() => myTweets);
+    };
+
+    const shouldDownloadVid = async ({ id }) => {
+        return not(haveIRepliedToTweetAlready)(id, await getMyTweets());
+    };
+
+    const fetchTweet = (tweetId) => {
+        return t.get(`statuses/show`, {
+            id: tweetId,
+            tweet_mode: 'extended',
+        }).then(r => {
+            if (r.data.errors) {
+                throw new TwitterErrorResponse('statuses/show', r.data.errors);
+            }
+            return r.data;
+        });
+    };
+
+    return {
+        getMentions,
+        reply,
+        replyWithLink,
+        shouldDownloadVid,
+        getTweets,
+        fetchTweet
+    };
+
+};
---
+++ src/services/tweet_operations.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const { get, SUCCESS, FAIL, UNCERTAIN } = require('../utils');
+const { ExternalPublisherError, NoVideoInTweet } = require('../errors');
+
+const isTweetAReply = (tweet) => !!tweet.in_reply_to_status_id_str;
+
+const isTweetAReplyToMe = (tweet) => tweet.in_reply_to_screen_name === process.env.TWITTER_SCREEN_NAME;
+
+const haveIRepliedToTweetAlready = (tweetId, myTweets) => {
+    return !!myTweets.find(t => t === tweetId);
+};
+
+const extractVideoLink = async (tweetObject, { cache, twitter }) => {
+    let cachedLink = await cache.getAsync(`tweet-${tweetObject.id_str}`);
+    if (cachedLink) {
+        return cachedLink;
+    }
+
+    // recursively check for a  link
+    function lookForLink(object) {
+        const keys = Object.keys(object);
+        for (let k of keys) {
+            if (typeof object[k] === 'string'
+                && object[k].startsWith('http')) {
+                return object[k];
+            } else if (typeof object[k] === 'object') {
+                let nestedLink = lookForLink(object[k]);
+                if (nestedLink) {
+                    return nestedLink;
+                }
+            }
+        }
+        return null;
+    }
+
+    try {
+        // the direct path
+        return tweetObject.extended_entities.media[0].video_info
+            .variants.find(variant => variant.content_type === 'video/mp4')
+            .url;
+    } catch (e) {
+        let additionalMediaInfo = get(tweetObject, 'extended_entities.media.0..additional_media_info');
+        if (additionalMediaInfo && !additionalMediaInfo.embeddable) {
+            // a custom publisher? not much we can do about it
+            // see https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/extended-entities-object.html
+            // We'll still try our best, though
+            let link = lookForLink(additionalMediaInfo);
+            if (!link) {
+                throw new ExternalPublisherError("Looks like this video's from an external publisher who's restricted it, so I can't access it :(", FAIL);
+            }
+            throw new ExternalPublisherError("Looks like this video's from an external publisher who's restricted it. Here's the best I could come up with: " + link, UNCERTAIN);
+
+        } else if (get(tweetObject, 'entities.media')) {
+            // sometimes, the tweet is a share of another tweet containing media
+            // example: https://twitter.com/GalacticoHD/status/889023844991807489
+            let expandedUrl = tweetObject.entities.media[0].expanded_url.split('/');
+            let tweetId = expandedUrl[expandedUrl.length - 3];
+            if (tweetId !== tweetObject.id_str) {
+                return await twitter.fetchTweet(tweetId)
+                    .then(t => extractVideoLink(t, { cache, twitter }));
+            }
+        }
+        throw new NoVideoInTweet(tweetObject);
+    }
+};
+
+const handleTweetProcessingError = (e, tweet, { cache, twitter }) => {
+    if (e.name === 'ExternalPublisherError') {
+        return twitter.reply(tweet, e.message).then(() => e.status);
+    }
+    console.log(`Failed processing tweet: ${JSON.stringify(tweet)} - Error: ${JSON.stringify(e)}`);
+    return cache.lpushAsync('Fail', [JSON.stringify(tweet)])
+        .then(() => e.name === 'NoVideoInTweet' ? null : FAIL);
+};
+
+const handleTweetProcessingSuccess = (tweet, link, { cache, twitter }) => {
+    return Promise.all([
+        cache.setAsync(`tweet-${tweet.referencing_tweet}`, link),
+        twitter.replyWithLink(tweet, link),
+    ]).then(() => SUCCESS);
+};
+
+module.exports = {
+    isTweetAReply,
+    isTweetAReplyToMe,
+    haveIRepliedToTweetAlready,
+    extractVideoLink,
+    handleTweetProcessingError,
+    handleTweetProcessingSuccess
+};

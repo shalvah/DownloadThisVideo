d8bd08d7c7fd87c75019b499574076cc348368ac
shalvah <diakon.ng@gmail.com>
2018-06-09 03:36:25 +0100

    Refactor utils

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,23 @@
 'use strict';
 
 const not = (fn) => (...args) => !fn(...args);
+
 const and = (...fns) => (...args) => fns.reduce((y, fn) => fn(...args) && y, true);
+
+const pluck = (values, key) => values.map(v => v[key]);
+
+const get = (object, path) => {
+    let lookup = Object.assign({}, object);
+    let keys = path.split('.');
+    for (let key of keys) {
+        if (lookup[key]) {
+            lookup = lookup[key];
+        } else {
+            return null;
+        }
+    }
+    return lookup;
+};
 
 const finish = (cb, cache) => {
     cache.quit();
@@ -13,6 +29,11 @@ const finish = (cb, cache) => {
                 body
             };
             cb(null, response);
+        },
+
+        fail(body) {
+            console.log(`Fail response: ${body}`);
+            cb(body);
         }
     }
 };
@@ -22,21 +43,43 @@ const randomSuccessResponse = () => {
         "Here you go!",
         "Here's the link you requested:",
         "Enjoy!",
+        "Yay, video!"
     ];
     return responses[Math.floor(Math.random() * responses.length)]
 };
 
-class CustomPublisherError extends Error {
+class ExternalPublisherError extends Error {
     constructor(message) {
         super(message);
-        this.name = 'CustomPublisherError';
+        this.name = 'ExternalPublisherError';
     }
 }
+
+class TwitterErrorResponse extends Error {
+    constructor(endpoint, errors) {
+        super('Error from Twitter API call');
+        this.name = 'TwitterErrorResponse';
+        this.errors = errors;
+        this.endpoint = endpoint;
+    }
+}
+
+const SUCCESS = 'Success';
+
+const FAIL = 'Fail';
+
+const UNCERTAIN = 'Uncertain';
 
 module.exports = {
     not,
     and,
+    pluck,
+    get,
     finish,
     randomSuccessResponse,
-    CustomPublisherError
+    ExternalPublisherError,
+    TwitterErrorResponse,
+    SUCCESS,
+    FAIL,
+    UNCERTAIN
 };

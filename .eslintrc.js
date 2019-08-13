9308de351fdcaad8f6662062dc871a7ca52511c9
shalvah <diakon.ng@gmail.com>
2019-08-13 22:36:05 +0100

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "no-unused-vars": ["warn"],
+        "no-await-in-loop": ["warn"],
+        "no-extra-semi": ["warn"],
+        "no-extra-boolean-cast": ["off"],
+        "no-inner-declarations": ["off"],
+        "no-mixed-spaces-and-tabs": ["off"],
+    }
+};

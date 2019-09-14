549fdf7f8ac865531097a2fb17bb0118f42237ce
shalvah <diakon.ng@gmail.com>
2019-09-14 15:52:40 +0100

    Switch to shared cache instance

--- handler.js
+++ handler.js
@@ -7,83 +7,91 @@ const ops = require('./src/services/tweet_operations');
 const makeCache = require('./src/services/factory.cache');
 const makeTwitter = require('./src/services/factory.twitter');
 
-module.exports.fetchTweetsToDownload = async (event, context, callback) => {
+(async () => {
     const cache = await makeCache();
     const twitter = makeTwitter(cache);
 
-    let lastTweetRetrieved = null;
-    let count = 0;
-    let mentions = await twitter.getMentions();
-    while (mentions.length) {
-        await sns.sendToSns(mentions);
-        lastTweetRetrieved = mentions[0].id;
-        count += mentions.length;
-        mentions = await twitter.getMentions(lastTweetRetrieved);
-    }
+    module.exports.fetchTweetsToDownload = async (event, context, callback) => {
+        // Since we're using a single cache connection,
+        // we permit this instance of the function to exit without waiting for the cache
+        context.callbackWaitsForEmptyEventLoop = false;
 
-    if (lastTweetRetrieved) {
-        await cache.setAsync('lastTweetRetrieved', lastTweetRetrieved);
-    }
-    finish(callback, cache).success(`Published ${count} tweets`);
-};
+        let lastTweetRetrieved = null;
+        let count = 0;
+        let mentions = await twitter.getMentions();
+        while (mentions.length) {
+            await sns.sendToSns(mentions);
+            lastTweetRetrieved = mentions[0].id;
+            count += mentions.length;
+            mentions = await twitter.getMentions(lastTweetRetrieved);
+        }
 
-module.exports.sendDownloadLink = async (event, context, callback) => {
-    const cache = await makeCache();
-    const twitter = makeTwitter(cache);
+        if (lastTweetRetrieved) {
+            await cache.setAsync('lastTweetRetrieved', lastTweetRetrieved);
+        }
+        finish(callback).success(`Published ${count} tweets`);
+    };
 
-    const tweets = sns.getPayloadFromSnsEvent(event);
-    const tweetObjects = await twitter.getActualTweetsReferenced(tweets);
-    let results = await Promise.all(tweetObjects.map((tweetObject) => {
-        let tweet = tweets.find(t => t.referencing_tweet === tweetObject.id_str);
-        return ops.extractVideoLink(tweetObject, {cache, twitter})
-            .then(link => ops.handleTweetProcessingSuccess(tweet, link, {cache, twitter}))
-            .catch(e => ops.handleTweetProcessingError(e, tweet, {cache, twitter, tweetObject}));
-    }));
+    module.exports.sendDownloadLink = async (event, context, callback) => {
+        const tweets = sns.getPayloadFromSnsEvent(event);
+        const tweetObjects = await twitter.getActualTweetsReferenced(tweets);
+        let results = await Promise.all(tweetObjects.map((tweetObject) => {
+            let tweet = tweets.find(t => t.referencing_tweet === tweetObject.id_str);
+            return ops.extractVideoLink(tweetObject, {cache, twitter})
+                .then(link => ops.handleTweetProcessingSuccess(tweet, link, {cache, twitter}))
+                .catch(e => ops.handleTweetProcessingError(e, tweet, {cache, twitter, tweetObject}));
+        }));
 
-    results = results.filter(r => r !== null);
-    cloudwatch.logResults(results);
-    finish(callback, cache).success(`Processed ${tweets.length} tasks`);
-};
+        results = results.filter(r => r !== null);
+        cloudwatch.logResults(results);
+        finish(callback).success(`Processed ${tweets.length} tasks`);
+    };
 
-module.exports.retryFailedTasks = async (event, context, callback) => {
-    const cache = await makeCache();
-    const tweets = await cache.lrangeAsync('Fail', 0, -1);
+    module.exports.retryFailedTasks = async (event, context, callback) => {
+        context.callbackWaitsForEmptyEventLoop = false;
 
-    if (!tweets.length) {
-        finish(callback, cache).success(`No tasks for retrying`);
-        return;
-    }
-    await sns.sendToSns(tweets.map(JSON.parse));
-    await cache.delAsync('Fail');
-    finish(callback, cache).success(`Sent ${tweets.length} tasks for retrying`);
-};
+        const tweets = await cache.lrangeAsync('Fail', 0, -1);
 
-module.exports.getDownloads = async (event, context, callback) => {
-    const username = event.pathParameters.username;
-    switch (username) {
-        case null:
-        case undefined:
-        case '':
-            finish(callback).render('home', { link: getSponsoredLink() });
+        if (!tweets.length) {
+            finish(callback).success(`No tasks for retrying`);
             return;
-        case 'faq':
-            const faqs = require('./faqs');
-            finish(callback).render('faq', { faqs, link: getSponsoredLink() });
-            return;
-        default:
-            const cache = await makeCache();
-            let downloads = await ops.getUserDownloads(cache, username);
-            const prepareDownloadforFrontend = (d) => {
-                return JSON.parse(d, function convertTimeToRelative(key, value) {
-                    return key === 'time' ? getRelativeTime(value) : value;
-                })
-            };
-            downloads = downloads.map(prepareDownloadforFrontend);
+        }
+        await sns.sendToSns(tweets.map(JSON.parse));
+        await cache.delAsync('Fail');
+        finish(callback).success(`Sent ${tweets.length} tasks for retrying`);
+    };
 
-            finish(callback, cache).render('downloads', {username, downloads, link: getSponsoredLink()});
-    }
-};
+    module.exports.getDownloads = async (event, context, callback) => {
+        context.callbackWaitsForEmptyEventLoop = false;
 
-module.exports.getHomePage = (event, context, callback) => {
-    finish(callback).render('home',  { link: getSponsoredLink() });
-};
+        const username = event.pathParameters.username;
+        switch (username) {
+            case null:
+            case undefined:
+            case '':
+                finish(callback).render('home', {link: getSponsoredLink()});
+                return;
+            case 'faq':
+                const faqs = require('./faqs');
+                finish(callback).render('faq', {faqs, link: getSponsoredLink()});
+                return;
+            default:
+                let downloads = await ops.getUserDownloads(cache, username);
+                const prepareDownloadforFrontend = (d) => {
+                    return JSON.parse(d, function convertTimeToRelative(key, value) {
+                        return key === 'time' ? getRelativeTime(value) : value;
+                    })
+                };
+                downloads = downloads.map(prepareDownloadforFrontend);
+
+                finish(callback).render('downloads', {username, downloads, link: getSponsoredLink()});
+        }
+    };
+
+    module.exports.getHomePage = (event, context, callback) => {
+        context.callbackWaitsForEmptyEventLoop = false;
+
+        finish(callback).render('home', {link: getSponsoredLink()});
+    };
+
+})();

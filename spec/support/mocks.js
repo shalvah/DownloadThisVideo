7104202f9e0ea0ea6149ce44fbec6d082dacbac7
shalvah <diakon.ng@gmail.com>
2019-10-05 21:59:54 +0100

    Add tests

---
+++ spec/support/mocks.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const mock = require('mock-require');
+
+const mockCache = () => {
+    const redis = require("redis-mock");
+    require('bluebird').promisifyAll(redis.RedisClient.prototype);
+    const cache = redis.createClient();
+    mock('../../src/services/cache', cache);
+    return cache;
+};
+
+const mockMetrics = () => {
+    mock('../../src/services/cloudwatch', {
+        logResults() { return Promise.resolve(); },
+    });
+};
+
+const mockTwitterAPI = () => {
+    const tls = require('tls');
+    // MITM converts HTTPS requests to HTTP, so we need to do this
+    // so we don't get TLS errors on the response
+    tls.TLSSocket.prototype.getPeerCertificate = (detailed) => null;
+    const Mitm = require("mitm");
+    const mitm = Mitm();
+    const requests = [];
+    mitm.on("connect", (socket, opts) => {
+        if (!opts.host.includes("twitter.com")) socket.bypass();
+    });
+    mitm.on("request", (req, res) => {
+        let rawData = '';
+        req.on("data", (chunk) => { console.log(chunk); rawData += chunk; })
+        if (req.url.includes("statuses/lookup")) {
+            req.on("end", () => {
+                const ids = require('url').parse(req.url, true).query.id;
+                requests.push(ids);
+                res.statusCode = 400;
+                const response = {errors: [{code:352,message:"Too many values."}]};
+                res.end(JSON.stringify(response));
+            });
+        }
+    });
+    return requests;
+};
+
+const mockSns = () => {
+    const sns = {
+        sendToSns(data) {
+        }
+    };
+    const mock = require('mock-require');
+    mock('../../src/services/sns', {
+        newReminderSet() {
+        }
+    });
+};
+
+module.exports = {
+    mockCache,
+    mockMetrics,
+    mockTwitterAPI,
+    mockSns,
+};
---
+++ spec/support/utils.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const createUser = (username = null, id = null) => {
+    return {
+        id: id || 6253282,
+        id_str: id || "6253282",
+        name: "Rando dude",
+        screen_name: username || "rando_dude12",
+        location: "San Francisco, CA",
+        profile_location: null,
+        description: "Meh",
+        entities: {},
+        protected: false,
+        followers_count: 6133636,
+        friends_count: 12,
+        listed_count: 12936,
+        created_at: "Wed May 23 06:01:13 +0000 2007",
+        favourites_count: 31,
+        utc_offset: null,
+        time_zone: null,
+        geo_enabled: null,
+        verified: true,
+        statuses_count: 3656,
+        lang: null,
+        contributors_enabled: null,
+        is_translator: null,
+        is_translation_enabled: null,
+        profile_background_color: null,
+        profile_background_image_url: null,
+        profile_background_image_url_https: null,
+        profile_background_tile: null,
+        profile_image_url: null,
+        profile_image_url_https: "https:\/\/pbs.twimg.com\/profile_images\/942858479592554497\/BbazLO9L_normal.jpg",
+        profile_banner_url: null,
+        profile_link_color: null,
+        profile_sidebar_border_color: null,
+        profile_sidebar_fill_color: null,
+        profile_text_color: null,
+        profile_use_background_image: null,
+        has_extended_profile: null,
+        default_profile: false,
+        default_profile_image: false,
+        following: null,
+        follow_request_sent: null,
+        notifications: null,
+        translator_type: null
+    };
+};
+
+const createTweet = ({ username = null, text = null, inReplyTo = null, retweetedTweet = null, date = new Date} = {}) => {
+    username = username || 'rando_dude12';
+    text = text || `@${username} Hehe, random.`;
+    text = inReplyTo ? `@${inReplyTo.user} ${text}` : text;
+    return {
+        created_at: require('moment')(date).format('ddd MMM D HH:mm:ss ZZ YYYY'), // something like 'Sun Sep 22 11:48:59 +0000 2019'
+        id: 8759849726758343547627,
+        id_str: '8759849726758343547627',
+        text,
+        display_text_range: [],
+        source: '<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>',
+        truncated: false,
+        in_reply_to_status_id: inReplyTo ? 34788 : null,
+        in_reply_to_status_id_str: inReplyTo ? inReplyTo.id : null,
+        in_reply_to_user_id: inReplyTo ? 5499273 : null,
+        in_reply_to_user_id_str: inReplyTo ? '5499273' : null,
+        in_reply_to_screen_name: inReplyTo ? inReplyTo.user : null,
+        user: createUser(username, '78564765'),
+        geo: null,
+        coordinates: null,
+        place: {},
+        contributors: null,
+        is_quote_status: false,
+        retweeted_status: retweetedTweet,
+        quote_count: 0,
+        reply_count: 0,
+        retweet_count: 0,
+        favorite_count: 0,
+        entities: {},
+        favorited: false,
+        retweeted: false,
+        filter_level: 'low',
+        lang: 'en',
+        timestamp_ms: '1569152939970'
+    }
+};
+
+const createRetweet = (opts = {}) => {
+    opts.retweetedTweet = createTweet();
+    return createTweet(opts);
+};
+
+const createSnsEvent = (numberOfTweets) => {
+    const tweetData = {
+        id: "3",
+        time: "Wed May 23 06:01:13 +0000 2007",
+        referencing_tweet: "3",
+        author: "some_dude",
+    };
+    const tweets = new Array(numberOfTweets).fill(tweetData);
+
+    const event = {
+        Records: [
+            {
+                Sns: {
+                    Message: JSON.stringify(tweets),
+                }
+            }
+        ]
+    };
+    return event;
+};
+
+module.exports = {
+    createTweet,
+    createRetweet,
+    createTweetCreateEvent,
+    createUser,
+    createWebhookEvent,
+    createSnsEvent,
+};
